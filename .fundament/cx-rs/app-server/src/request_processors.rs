93c475f7f4a680fe3f357aff840b3880151556dc
vladLeopold <leopolds2010@gmail.com>
2026-09-10 01:02:54 +0300

    fix: drop bedrock_auth module declaration

--- .fundament/cx-rs/app-server/src/request_processors.rs
+++ .fundament/cx-rs/app-server/src/request_processors.rs
@@ -530,7 +530,6 @@ use cx_app_server_protocol::ServerRequest;
 
 mod account_processor;
 mod apps_processor;
-mod bedrock_auth;
 mod catalog_processor;
 mod command_exec_processor;
 mod config_processor;
